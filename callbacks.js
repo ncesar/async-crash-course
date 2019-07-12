5fcf04a5e87854533760339b1de5877fdf5a8857
César Nascimento <ceesar90@gmail.com>
2019-07-12 14:31:59 -0300

    Update callbacks.js

--- callbacks.js
+++ callbacks.js
@@ -9,40 +9,37 @@
 
 //CALLBACKS
 
-// const posts = [
-//     { 
-//   title: 'Post One',
-//   body: 'This is post one'
-//     },
-//     { 
-//   title: 'Post Two',
-//   body: 'This is post Two'
-//     }
-// ];
+const posts = [
+    { 
+  title: 'Post One',
+  body: 'This is post one'
+    },
+    { 
+  title: 'Post Two',
+  body: 'This is post Two'
+    }
+];
+
+function getPosts() {
+  setTimeout(() => {
+    let output = '';
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post, callback) {
+  setTimeout(() => {
+    posts.push(post);
+    callback();
+  }, 2000);
+}
+//a ideia é que o callback como parametro,
+//so vai rodar depois do push(pq ta embaixo dele)
+//entao primeiro o push é feito, e so depois
+//o callback vem
 
 
-
-//FUNÇÃO SEM ASYNC
-
-
-
-// function getPosts() {
-//   setTimeout(() => {
-//     let output = '';
-//     posts.forEach((post, index) => {
-//       output += `<li>${post.title}</li>`;
-//     });
-//     document.body.innerHTML = output;
-//   }, 1000);
-// }
-
-// function createPost(post) {
-//   setTimeout(() => {
-//     posts.push(post);
-//   }, 2000);
-// }
-
-// getPosts();
-
-
-// createpost({ title: 'Post Three', body: 'This is post three' });
+createPost({ title: 'Post Three', body: 'This is post three' }, getPosts);
